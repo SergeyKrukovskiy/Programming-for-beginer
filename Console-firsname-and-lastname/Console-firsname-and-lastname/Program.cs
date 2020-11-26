e7c85fb6396c6667314d126cecba28e6d173a69a
Sergey <serezha.cruckowsckij@yandex.ru>
2020-11-26 13:22:43 +0700

    Консольная версия
    +изменение расположения первой программы

---
+++ Console-firsname-and-lastname/Console-firsname-and-lastname/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Console_firsname_and_lastname
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите фамилию");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Введите имя");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Приятно познакомится " + lastname + " " + firstname);
+            Console.ReadLine();
+
+        }
+    }
+}
