e4280efd7cc5a2cf40a59779f5cf09c0236597bb
Sergey <serezha.cruckowsckij@yandex.ru>
2020-11-26 13:34:27 +0700

    Возраст и имя

---
+++ Console-firstname-age/Console-firstname-age/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Console_firstname_age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите Возраст");
+            string age = Console.ReadLine();
+            Console.WriteLine("Приятно познакомится " + name + " " + age + " лет.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dialog-windows-firstname-age/Dialog-windows-firstname-age/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Dialog_windows_firstname_age
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string firstname = Interaction.InputBox("Как вас зовут?", "Введите имя...");
+            string age = Interaction.InputBox("Сколько вам лет?", "Введите фамилию...");
+            MessageBox.Show("Здравствуйте " + firstname + " " + age + " лет.", "Приятно познакомится!");
+        }
+    }
+}
