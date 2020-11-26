ffaef7994e2d0125ed94b3bb44a83dcaa2fbb57d
Sergey <serezha.cruckowsckij@yandex.ru>
2020-11-26 13:14:36 +0700

    Имя фамилия с помощью диалоговых окон

---
+++ Programming-for-beginer/Programming-for-beginer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Programming_for_beginer
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstname = Interaction.InputBox("Как вас зовут?", "Введите имя...");
+            string lastname = Interaction.InputBox("Как ваша фамилия?", "Введите фамилию...");
+            MessageBox.Show("Здравствуйте " + firstname + " " + lastname,"Приятно познакомится!");
+        }
+    }
+}
