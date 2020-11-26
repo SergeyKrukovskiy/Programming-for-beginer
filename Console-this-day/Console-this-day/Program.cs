6ee75bde6a99e8b83401f53491a0889a08a3f9b3
Sergey <serezha.cruckowsckij@yandex.ru>
2020-11-26 13:52:54 +0700

    Вывод текущей даты

---
+++ Console-this-day/Console-this-day/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Console_this_day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] str = Console.ReadLine().Split(' ');
+            Console.WriteLine(str[0] + " " + str[2] + " " + str[1]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dialog-windows-this-day/Dialog-windows-this-day/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Dialog_windows_this_day
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Interaction.InputBox("Введите сегодняшнюю дату: ", "Дата?").Split(' ');
+            MessageBox.Show(str[0] + " " + str[2] + " " + str[1], "Дата");
+        }
+    }
+}
